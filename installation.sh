e8f9bc2c7b3d214394355a3d315146d6402fb299
Phani Krishna <krishnaphaniiitg@gmail.com>
2019-06-24 16:06:33 +0900

    Update and rename installation.txt to installation.sh

---
+++ installation.sh
@@ -0,0 +1,15 @@
+#install jupyter 
+pip3 install setuptools 
+pip3 install  jupyter_contrib_nbextensions \
+    jupyter_nbextensions_configurator
+
+jupyter contrib nbextension install --user && \
+    jupyter nbextensions_configurator enable --user
+
+# port opening alias
+echo "alias jp='f(){jupyter notebook --no-browser --ip 0.0.0.0 --port \$1 --NotebookApp.token=''};f'" >> ~/.zshrc
+
+
+mkdir ~/.jupyter/custom
+mv ~/jupyter_utils/custom.js  ~/.jupyter/custom/.
+mv ~/jupyter_utils/notebook.json ~/.jupyter/nbconfig/notebook.json
