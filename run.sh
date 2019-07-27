c2e3f6dab5f7d0aa3882f28b4e4c249c71677219
Phani Krishna <krishnaphaniiitg@gmail.com>
2019-07-27 16:03:32 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
-#install jupyter 
-pip3 install setuptools 
-pip3 install  jupyter_contrib_nbextensions \
-    jupyter_nbextensions_configurator
 
-jupyter contrib nbextension install --user && \
-    jupyter nbextensions_configurator enable --user
 
 # port opening alias
 echo "alias jp='f(){jupyter notebook --no-browser --ip 0.0.0.0 --port \$1 --NotebookApp.token=''};f'" >> ~/.zshrc
