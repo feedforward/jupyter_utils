5805ea2d4bdda7394d0e6cb09e57039e777710a7
Phani Krishna <krishnaphaniiitg@gmail.com>
2019-08-29 19:30:25 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 
 
 # port opening alias
-echo "alias jp='f(){jupyter notebook --no-browser --ip 0.0.0.0 --port \$1 --NotebookApp.token=''};f'" >> ~/.zshrc
-
+#echo "alias jp='f(){jupyter notebook --no-browser --ip 0.0.0.0 --port \$1 --NotebookApp.token=''};f'" >> ~/.zshrc
+echo "alias jp='f(){jupyter notebook --no-browser --port \$1};f'" >> ~/.zshrc
 
 mkdir ~/.jupyter/custom
 cp ~/jupyter_utils/custom.js  ~/.jupyter/custom/.
