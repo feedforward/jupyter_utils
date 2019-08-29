5e5f5b013a7a073416e252e76ed030cbfb8622fd
Phani Krishna <krishnaphaniiitg@gmail.com>
2019-08-29 19:30:54 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 
 
-# port opening alias
+# port opening alias for kubernetes pod
 #echo "alias jp='f(){jupyter notebook --no-browser --ip 0.0.0.0 --port \$1 --NotebookApp.token=''};f'" >> ~/.zshrc
+# Port opening in a remote server
 echo "alias jp='f(){jupyter notebook --no-browser --port \$1};f'" >> ~/.zshrc
 
 mkdir ~/.jupyter/custom
